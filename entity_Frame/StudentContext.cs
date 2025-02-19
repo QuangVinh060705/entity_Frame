425d6cd11e8547cbb65345925ceaf03b102290da
QuangVinh <quangvinh.dinhnguyen0607@gmail.com>
2025-02-19 15:45:22 +0700

    Add project files.

---
+++ entity_Frame/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_Frame
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("name=StudentDBConnectionString")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
